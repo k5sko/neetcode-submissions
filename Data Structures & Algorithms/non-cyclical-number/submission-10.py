618722c4cbbb8967f2c482a2b0d4374597c903b4
k5sko <70296216+k5sko@users.noreply.github.com>
2026-07-09 16:28:36 -0700

    Add: non-cyclical-number - submission-10

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-10.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        """ Let's try Floyd's tortoise and hare algorithm
+
+        What we need: 
+        1. fast/slow pointer
+        2. make fast pointer move at exactly 1 pointer relative to the slower pointer
+        3. check if they meet
+        """
+
+        # slow, fast = n, n
+        # wheres the implicit graph structure?
+        slow, fast = n, n
+
+        slow = sumSquare(slow)
+        fast = sumSquare(sumSquare(fast))
+
+        while fast != slow:
+            slow = sumSquare(slow)
+            fast = sumSquare(sumSquare(fast))
+
+        return fast == 1
+        
+
+    def sumSquare(self, n: int) -> int:
+        res = 0
+
+        while n > 0:
+            res += n%10
+            n //= 10
+        
+        return res
