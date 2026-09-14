35aeda8eeff133c443450224b1570282089bb016
k5sko <70296216+k5sko@users.noreply.github.com>
2026-09-14 08:47:11 -0700

    Add: anagram-groups - submission-22

---
+++ Data Structures & Algorithms/anagram-groups/submission-22.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = dict()
+        for string in strs:
+            order = ""
+            
+            for char in sorted(string):
+                order += char
+
+            if order not in anagrams:
+                anagrams[order] = []
+
+            anagrams[order].append(string)
+
+        return list(anagrams.values())
