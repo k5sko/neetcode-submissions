172fa826555e24bb1bace59b433630d1a1768fd0
k5sko <70296216+k5sko@users.noreply.github.com>
2026-09-14 17:33:19 -0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # binary search for the row by looking within bounds
+        l, r = 0, len(matrix) - 1
+        while l <= r:
+            m = (l + r)//2
+            if matrix[m][0] <= target and target <= matrix[m][-1]:
+                r = m
+                break
+            elif matrix[m][0] > target:
+                r = m - 1
+            else:
+                l = m + 1
+
+        if r < 0 or matrix[r][0] > target or matrix[r][-1] < target:
+            return False
+
+        # now we know r is the correct row
+
+        i, j = 0, len(matrix[r]) - 1
+        while i < j:
+            m = (i + j)//2
+            if matrix[r][m] == target:
+                j = m
+                break
+            elif matrix[r][m] > target:
+                j = m - 1
+            else:
+                i = m + 1
+
+        return matrix[r][j] == target
