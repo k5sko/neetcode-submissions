b4b0d7ecf7c5d966d67468004f60aec6a44d8bef
k5sko <70296216+k5sko@users.noreply.github.com>
2026-09-21 17:54:04 -0700

    Add: spiral-matrix - submission-6

---
+++ Data Structures & Algorithms/spiral-matrix/submission-6.py
@@ -0,0 +1,32 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if len(matrix) == 0 or len(matrix[0]) == 0:
+            return []
+
+        if len(matrix) == 1:
+            return matrix[0]
+
+        if len(matrix[0]) == 1:
+            return [matrix[i][0] for i in range(len(matrix[0]))]
+        
+        elems = [matrix[0][0]]
+
+        for i in range(1, len(matrix[0]) - 1):
+            elems += [matrix[0][i]]
+
+        elems += [matrix[0][-1]]
+
+        for i in range(1, len(matrix) - 1):
+            elems += [matrix[i][-1]]
+
+        elems += [matrix[-1][-1]]
+
+        for i in range(len(matrix[-1]) - 2, 0, -1):
+            elems += [matrix[-1][i]]
+
+        elems += [matrix[-1][0]]
+
+        for i in range(len(matrix) - 2, 0, -1):
+            elems += [matrix[i][0]]
+
+        return elems + self.spiralOrder([mat[1:-1] for mat in matrix[1:-1]])
